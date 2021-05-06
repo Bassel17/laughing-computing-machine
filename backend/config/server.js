596f39d03cfebd07625ae00335e09aa9cbcd14ba
Bassel Kanso <basselkanso82@gmail.com>
2021-05-07 00:16:50 +0300

    prepare project files

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '85704af1550404d88bcc7ad9c17541ad'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '89f32fd6-fc1a-4421-8b6d-280fa2b8e6a3'
+};
